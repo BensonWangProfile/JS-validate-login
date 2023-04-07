3c732c673937fed47d4080faf13f022185ae282c
benson <benson95217@gmail.com>
2023-04-07 15:17:33 +0800

    Modify: 修改監聽事件change -> input

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ console.log("inputs", inputs); // NodeList [inputs#email, inputs#password, input
 // 讓每個 input 綁定監聽
 for (let i = 0; i < inputs.length; i++) {
   console.log(inputs.item(i));
-  inputs.item(i).addEventListener("change", () => {
+  inputs.item(i).addEventListener("input", () => {
     let errors = validate(form, constraints) || {}; // 顯示所有input的問題｜🔥為什麼 form 可以直接放進去
     // console.log(errors);
     let currentDom = inputs.item(i);
